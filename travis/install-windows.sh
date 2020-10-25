87f8c4ce8545ec1420d3fa55d67c765562819f65
Tom van Dijk <tom@tvandijk.nl>
2020-10-25 10:14:39 +0100

    attempt to fix windows build

--- travis/install-windows.sh
+++ travis/install-windows.sh
@@ -7,7 +7,7 @@ export BUILD_HOST="x86_64-w64-mingw32.static"
 export CC="x86_64-w64-mingw32.static-gcc"
 export CXX="x86_64-w64-mingw32.static-g++"
 
-echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" \
+echo "deb http://pkg.mxe.cc/repos/apt xenial main" \
     | sudo tee /etc/apt/sources.list.d/mxeapt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
     --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
