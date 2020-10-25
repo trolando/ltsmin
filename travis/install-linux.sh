460bc275ea3ec898dc63ff3132c341dc52f54b06
Tom van Dijk <tom@tvandijk.nl>
2020-10-25 09:40:43 +0100

    Work in progress upgrading Ubuntu builds

--- travis/install-linux.sh
+++ travis/install-linux.sh
@@ -6,11 +6,11 @@ export GHCVER="7.10.3"
 export HAPPYVER="1.19.5"
 
 # set correct compiler
-export CC="gcc-6"
-export CXX="g++-6"
-export AR="gcc-ar-6"
-export RANLIB="gcc-ranlib-6"
-export NM="gcc-nm-6"
+# export CC="gcc-6"
+# export CXX="g++-6"
+# export AR="gcc-ar-6"
+# export RANLIB="gcc-ranlib-6"
+# export NM="gcc-nm-6"
 
 travis/install-generic.sh
 
